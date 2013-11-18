f1330849b85253b6cc4338b0f3ca03e85b17a867
Hugo Magalhaes <hugomn@gmail.com>
2013-11-18 15:35:07 -0200

    Traducao do campo de busca

--- wp-content/themes/vamosfalar/searchform.php
+++ wp-content/themes/vamosfalar/searchform.php
@@ -7,5 +7,5 @@
  */
 ?>
 <form method="get" class="main-searchbar" action="<?php echo home_url( '/' ); ?>">
-<input type="search" name="s" value="<?php _e( 'Type and hit enter to search...', 'wpex' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
+<input type="search" name="s" value="<?php _e( 'Digite e aperte enter para buscar...', 'wpex' ); ?>" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;">
 </form>
